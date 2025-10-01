d16c0ba81efc1f62e7aa46af453cef0b61644d58
JavviFdeez <javifdez195@gmail.com>
2025-10-01 08:51:13 +0200

    DESARROLLO SiteMap y robots.txt
    *

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://www.javvi.dev',
+  integrations: [sitemap()],
 });
