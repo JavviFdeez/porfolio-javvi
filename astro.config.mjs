8183b485efc01eb0f04cdb7553ab44ccf4debce6
JavviFdeez <javifdez195@gmail.com>
2025-11-30 13:28:27 +0100

    DESARROLLO Vercel
    * Agregar adaptador de Vercel para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://www.javvi.dev',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'server',
 });
